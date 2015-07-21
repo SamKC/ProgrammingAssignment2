f80c93285186ab0f4238405a958bdb4754254dbf
SamKC <kin_sambath@yahoo.com>
2015-07-20 21:51:40 -0400

    changed some codes and comments on the example part

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,14 @@
 <<<<<<< HEAD
 
 ## While we want to reuse the value of the inverse matrix again, recomputation may 
-## be taken a long time to do agian, especially in the case of repeated 
-## computation, for example, in a loop. It may be more beneficial and convenient to 
-## cache the value of the inverse matrixin such a way that we can look up in the cache 
-## rather than recomputed. The two functions below are expressed to cache the value 
-## of the inverse matrix.
+## be taken a long time to do agian, especially in the case of repeated computation,
+## for example, in a loop. It may be more beneficial and convenient to cache the value
+## of the inverse matrix in such a way that we can look up in the cache rather than recomputed.
+## The two functions below are expressed to cache the value of the inverse matrix. 
 
 
-## The first function, makeCacheMatrix creates a special "Matrix", which is  
-## a list containing a function to :
+## The first function, makeCacheMatrix creates a special "Matrix", which is a list containing a function to : 
+
 ## 1.set the value of the matrix named "invMat"
 ## 2.get the value of the matrix named "invMat"
 ## 3.set the value of the inverse matrix
@@ -30,12 +29,12 @@ makeCacheMatrix <- function(x = matrix()) {
     
 }
 
-## The second function, cacheSolve solves and get the result of the inverse  
-## of the matrix with the above function. Firstly, the function checks to see whether
-## the inverse matrix is already solved. If so, then it shows the massage, gets 
-## the inverse from cache and skips the computations. Otherwise it solves the inverse 
-## matrix from data and and sets the value of inverse matrix in cache via the setinverse
-## function.
+## The second function, cacheSolve solves and gets a result of the inverse of the matrix 
+## with the above function. Firstly, the function checks to see whether the inverse matrix 
+## is already solved. If so, then it shows the massage, gets the inverse from cache and skips  
+## the computation. Otherwise it solves the inverse matrix from data and and sets the value 
+## of inverse matrix in cache via the setinverse function.
+ 
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
@@ -58,9 +57,8 @@ cacheSolve <- function(x, ...) {
 ## cacheSolve(cacheInv) (note: no cache yet because no data store)
 ## cacheSolve(cacheInv) (note: if you redo, it caches by showing the massage)
 
-mat_dim2<-matrix(rnorm(10),2,2)
-print(mat_dim2)
-cacheInv = makeCacheMatrix(mat_dim2)
-m$get()
-cacheSolve(cacheInv)
-cacheSolve(cacheInv)
+mat_dim2<-matrix(1:4,2,2) ## matrix has to be a square matrix
+print(mat_dim2)           
+cacheInv = makeCacheMatrix(mat_dim2) 
+cacheSolve(cacheInv) ## no cache yet because no data store
+cacheSolve(cacheInv) ## if you redo, it caches by showing the massage and the result
