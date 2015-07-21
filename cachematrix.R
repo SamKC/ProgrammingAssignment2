bebbed35a2d4bb81364a2b9292622af443f89b12
SamKC <kin_sambath@yahoo.com>
2015-07-20 21:35:10 -0400

    Edit some words of introduction

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 <<<<<<< HEAD
 
-## While we want to reused the value of the inverse matrix again, recomputation may 
-## be challenged with potential time-consuming, especially in the case of repeated 
+## While we want to reuse the value of the inverse matrix again, recomputation may 
+## be taken a long time to do agian, especially in the case of repeated 
 ## computation, for example, in a loop. It may be more beneficial and convenient to 
 ## cache the value of the inverse matrixin such a way that we can look up in the cache 
 ## rather than recomputed. The two functions below are expressed to cache the value 
@@ -10,20 +10,20 @@
 
 ## The first function, makeCacheMatrix creates a special "Matrix", which is  
 ## a list containing a function to :
-## 1.set the value of the matrix named "inv"
-## 2.get the value of the matrix named "inv"
+## 1.set the value of the matrix named "invMat"
+## 2.get the value of the matrix named "invMat"
 ## 3.set the value of the inverse matrix
 ## 4.get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-    inv <- NULL
+    invMat <- NULL
     set <- function(y) {
         x <<- y
-        inv <<- NULL
+        invMat <<- NULL
     }
     get <- function() x
-    setinverse <- function(inverse) inv <<- inverse
-    getinverse <- function() inv
+    setinverse <- function(inverse) invMat <<- inverse
+    getinverse <- function() invMat
     list(set = set, get = get,
          setinverse = setinverse,
          getinverse = getinverse)
@@ -39,15 +39,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
-    inv <- x$getinverse()
-    if(!is.null(inv)) {
+    invMat <- x$getinverse()
+    if(!is.null(invMat)) {
         message("getting cached data")
-        return(inv)
+        return(invMat)
     }
     data <- x$get()
-    inv <- solve(data, ...)
-    x$setinverse(inv)
-    inv
+    invMat <- solve(data, ...)
+    x$setinverse(invMat)
+    invMat
 }
 
 ## The followings are the examples of how to use these functions:
